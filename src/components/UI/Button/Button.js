f975f81d7725d1ccc2a09c14ad2e2ea32e38a090
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-18 10:35:24 -0700

    complete: 'styled components & media'

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,10 @@
 // 77. Introducing Styled Components //
 
 import styled from 'styled-components';
+// line 7 & 16 - 79. Styled Components & Media Queries //
 
 const Button = styled.button`
+  width: 100%; // for web application 'add goal'
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -11,6 +13,9 @@ const Button = styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
   
+  @media (min-width: 768px) {
+    width: auto;   // for mobile application 'add goal'
+  }
 
   &:focus {
     outline: none;
